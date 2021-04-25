3672b5b9bffd42a8f36b37e1fa4d152154bf946e
Diwas Atreya <74746579+diwasatreya@users.noreply.github.com>
2021-04-25 12:09:39 +0545

    Create purge.js

---
+++ commands/moderation/purge.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: "clear",
+  aliases:["c", "purge"],
+  category: "moderation",
+  description: "Delete bulk messages with 1 command",
+  run: async (client, message, args) => {
+    
+            
+    if (message.deletable) {
+        message.delete();
+    }
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        return message.reply("Missing Permissions!").then(m => m.delete(5000));
+    }
+
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        return message.reply("This is not a number").then(m => m.delete(5000));
+    }
+
+    let deleteAmount;
+    if (parseInt(args[0]) > 100) {
+        deleteAmount = 100;
+    } else {
+        deleteAmount = parseInt(args[0]);
+    }
+
+    message.channel.bulkDelete(deleteAmount, true)
+    .catch(err => message.reply(`Something went wrong... ${err}`));
+    
+  }
+}
