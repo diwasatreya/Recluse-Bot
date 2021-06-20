bb1f933ee4c0cd644532a9f9dd313234a57f984a
Atreya YT <74746579+diwasatreya@users.noreply.github.com>
2021-06-20 22:41:14 +0545

    Update config.js

--- config.js
+++ config.js
@@ -12,4 +12,4 @@ exports.giphy_API = 'CaJxgWS4z2CImmnPsbfLBuGTWLkmElVx';
 exports.AME_API = '6f5f59280172426c821ece5adf83ac01877c0f7ad59946bf66a72c5addd2818907597db29d5512ba1170a9fbfee287bc68a755ae0306d137993a43a28393a040';
 
 exports.blague_API = 'rwfcDdWYMrMfdy9WvihcQeEO1-R_s1eh7hNttZFIzPEq5EZDcH2g3WiaO0ccyVml';
-ODQ5NTYxODQ0MDg3NjUyMzcy.YLc-Hw.MDigJPt4U6kS7yzD04dNb8L5I7I
+
